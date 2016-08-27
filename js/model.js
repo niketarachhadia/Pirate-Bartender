80c54507b5de1ee2e61b2394b126ec98b9584f93
niketarachhadia <niketa.rachhadia@gmail.com>
2016-08-27 16:24:51 -0500

    errors fixed

--- js/model.js
+++ js/model.js
@@ -2,7 +2,7 @@ var QuestionList = function(){
 	this.questions ={'strong':'Do you like your drinks strong?',
 					  'salty':'Do you like it with a salty tang?',
 					  'bitter':'Are you a lubber who likes it bitter?',
-					  'sweetness':'Would you like a bit of sweetness with your poison?',
+					  'sweet':'Would you like a bit of sweetness with your poison?',
 					  'fruity':'Are you one for a fruity finish?'};
 	};
 
@@ -10,7 +10,7 @@ var Pantry = function(){
 	this.ingredients = {'strong':['Glug of rum','slug of whisky','splash of gin'],
 				   'salty':['Olive on a stick','salt-dusted rim','rasher of bacon'],
 				   'bitter':['Shake of bitters','splash of tonic','twist of lemon peel'],
-				   'sweeet':['Sugar cube','spoonful of honey','splash of cola'],
+				   'sweet':['Sugar cube','spoonful of honey','splash of cola'],
 				   'fruity':['Slice of orange','dash of cassis','cherry on top']
 				   };
 	this.drink = [];	
